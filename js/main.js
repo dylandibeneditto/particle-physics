1709bc31fbc3abbbed4f573bb9461518c34f1e57
dylan <justbeawesome9@gmail.com>
2024-01-27 02:43:24 -0500

    working snow example - basic gravity

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import Board from "./board.js";
 import Display from "./display.js"
 
-let board = new Board(5)
-board.write([10,10], 1)
+let board = new Board(10)
+setInterval(()=> {
+    board.write([Math.floor(Math.random()*board.w), Math.floor(Math.random()*board.h)], 1);
+}, 10)
 let display = new Display(document.getElementById("canvas"), board)
 display.startAnimation()

233fde9cb5bb9dda610f4adc394bffc8aef99a1a
dylan <justbeawesome9@gmail.com>
2024-01-26 22:13:37 -0500

    locate function to convert 1d index to 2d

--- js/board.js
+++ js/board.js
@@ -1,18 +1,39 @@
 export default class Board {
     constructor(resolution) {
-        // input
+        /* input */
         this.res = resolution;
 
-        // commonly referenced values
-        this.w = window.innerWidth / this.res;
-        this.h = window.innerHeight / this.res;
+        /* commonly referenced values */
 
-        // private storage
-        this.array = new Array(Math.floor(this.w * this.h))
+        // starts indexing at zero
+        this.w = Math.floor(window.innerWidth / this.res)-1;
+        this.h = Math.floor(window.innerHeight / this.res)-1;
+
+        /* private storage */
+        this.array = new Array((this.w) * (this.h))
+
+        console.log(this.locate(this.w, this.h));
     }
 
+    // reinitializes width and height variables
+
     resize() {
-        this.w = window.innerWidth / this.res;
-        this.h = window.innerHeight / this.res
+        this.w = Math.floor(window.innerWidth / this.res)-1;
+        this.h = Math.floor(window.innerHeight / this.res)-1;
+    }
+
+    
+    /* justified with origin in top left corner
+     * starts indexing at values (0,0)
+     * returns index which can be used within other functions  */
+    locate(x,y) {
+
+        // check if number is within bounds
+        if((x>=0 && x<=this.w) && (y>=0 && y<=this.h)) {
+            return x+((y-1)*this.w)
+        } else {
+            throw new Error(`\nInputted coordinate (${x}, ${y})\n is not within boundaries of (width: ${this.w}, height: ${this.h})`)
+        }
+        
     }
 }

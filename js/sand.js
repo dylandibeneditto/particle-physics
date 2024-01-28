a1bc53423f33f8b167bcbded47d0ca89cd7e575e
dylan <justbeawesome9@gmail.com>
2024-01-27 19:56:31 -0500

    fixed sand algorithm lefthand bias

--- js/sand.js
+++ js/sand.js
@@ -12,27 +12,27 @@ export default class Sand extends Particle {
     /* searches in 3x1 kernel around x,y coords
      * runs swap if conditions for gravity, collision, etc. are met  */
     findDestination(x, y) {
-        // hard coded kernel seach pattern
-        // in order; BOTTOM LEFT, BOTTOM, BOTTOM RIGHT
-        const kernel = [[x - 1, y + 1], [x, y + 1], [x + 1, y + 1]]
+        // hard coded kernel pattern
+        // in order: BOTTOM LEFT, BOTTOM, BOTTOM RIGHT
+        const kernel = [[x - 1, y + 1], [x, y + 1], [x + 1, y + 1]];
 
         if (this.board.inBounds(...kernel[1])) {
             if (!this.board.value(...kernel[1])) {
                 return { position: kernel[1], offset: this.board.index(...kernel[1]) }
-            } else {
-                if (this.board.inBounds(...kernel[0])) {
-                    if (!this.board.value(...kernel[0])) {
-                        return { position: kernel[0], offset: this.board.index(...kernel[0]) }
+            } else if (this.board.inBounds(...kernel[0])) {
+                if (!this.board.value(...kernel[0])) {
+                    if (this.board.inBounds(...kernel[2]) && !this.board.value(...kernel[2])) {
+                        const r = (Math.round(Math.random()) * 2)
+                        console.log(r)
+                        return { position: kernel[r], offset: this.board.index(...kernel[r]) }
                     }
+                    return { position: kernel[0], offset: this.board.index(...kernel[0]) }
                 }
-
-                if (this.board.inBounds(...kernel[2])) {
-                    if (!this.board.value(...kernel[2])) {
-                        return { position: kernel[2], offset: this.board.index(...kernel[2]) }
-                    }
+            } else if (this.board.inBounds(...kernel[2])) {
+                if (!this.board.value(...kernel[2])) {
+                    return { position: kernel[2], offset: this.board.index(...kernel[2]) }
                 }
             }
         }
-
     }
 }

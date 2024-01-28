4bf50c0810f3a1766bba4e3f58065efc1124fdf2
dylan <justbeawesome9@gmail.com>
2024-01-27 19:20:52 -0500

    formatting

--- js/sand.js
+++ js/sand.js
@@ -5,7 +5,7 @@ export default class Sand extends Particle {
     constructor(options) {
         super(options)
         this.name = "sand";
-        this.wet = false;   
+        this.wet = false;
         this.board = new Logic().board;
     }
 
@@ -18,17 +18,17 @@ export default class Sand extends Particle {
 
         if (this.board.inBounds(...kernel[1])) {
             if (!this.board.value(...kernel[1])) {
-                return {position: kernel[1], offset: this.board.index(...kernel[1])}
+                return { position: kernel[1], offset: this.board.index(...kernel[1]) }
             } else {
                 if (this.board.inBounds(...kernel[0])) {
-                    if(!this.board.value(...kernel[0])) {
-                        return {position: kernel[0], offset: this.board.index(...kernel[0])}
+                    if (!this.board.value(...kernel[0])) {
+                        return { position: kernel[0], offset: this.board.index(...kernel[0]) }
                     }
                 }
 
-                if(this.board.inBounds(...kernel[2])) {
-                    if(!this.board.value(...kernel[2])) {
-                        return {position: kernel[2], offset: this.board.index(...kernel[2])}
+                if (this.board.inBounds(...kernel[2])) {
+                    if (!this.board.value(...kernel[2])) {
+                        return { position: kernel[2], offset: this.board.index(...kernel[2]) }
                     }
                 }
             }

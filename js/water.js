23fba08678c8ed42e7fb35a6fd310f6235e4022f
dylan <justbeawesome9@gmail.com>
2024-01-28 18:56:28 -0500

    water physics

--- js/water.js
+++ js/water.js
@@ -37,27 +37,34 @@ export default class Water extends Particle {
     /* searches in 3x2 kernel around x,y coords
      * runs swap if conditions are met  */
     findDestination(x, y) {
-        // hard coded kernel pattern
-        // in order: LEFT, BOTTOM LEFT, BOTTOM, BOTTOM RIGHT, RIGHT
-        const kernel = [[x - 1, y], [x - 1, y + 1], [x, y + 1], [x + 1, y + 1], [x + 1, y]];
+        let p1, p2, p3, p4, p5;
+        if (y + 1 < this.board.size) p1 = (((y) * this.board.size) + x);
+        if (y + 1 < this.board.size && x + 1 < this.board.size) p2 = (((y + 1) * this.board.size) + x + 1);
+        if (y + 1 < this.board.size && x - 1 >= 0) p3 = (((y + 1) * this.board.size) + x - 1);
+        if (x + 1 < this.board.size) p4 = ((y * this.board.size) + x + 1);
+        if (x - 1 >= 0) p5 = ((y * this.board.size));
 
-        if (this.board.inBounds(...kernel[2])) {
-            if (!this.board.value(...kernel[2])) {
-                return { position: kernel[2], offset: this.board.index(...kernel[2]) }
-            } else if (this.board.inBounds(...kernel[1])) {
-                if (!this.board.value(...kernel[1])) {
-                    if (this.board.inBounds(...kernel[3]) && !this.board.value(...kernel[3])) {
-                        const r = (Math.round(Math.random()) * 2) + 1
-                        console.log(r)
-                        return { position: kernel[r], offset: this.board.index(...kernel[r]) }
-                    }
-                    return { position: kernel[1], offset: this.board.index(...kernel[1]) }
-                }
-            } else if (this.board.inBounds(...kernel[3])) {
-                if (!this.board.value(...kernel[3])) {
-                    return { position: kernel[3], offset: this.board.index(...kernel[3]) }
-                }
-            }
+
+        if (p1 && !this.board.array[p1]) {
+
+            return { position: [x,y+1], offset: p1 };
+
+        } else if (p2 && !this.board.array[p2]) {
+
+            return { position: [x+1,y+1], offset: p2 };
+
+        } else if (p3 && !this.board.array[p3]) {
+
+            return { position: [x-1,y+1], offset: p3 };
+
+        } else if (p4 && !this.board.array[p4]) {
+
+            return { position: [x+1,y], offset: p4 };
+
+        } else if (p5 && !this.board.array[p5]) {
+
+            return { position: [x-1,y], offset: p5 };
+
         }
     }
 }
